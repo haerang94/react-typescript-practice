83580e52334088abd316c36e483d714b75e9e758
haerang <gofkdvjvl@naver.com>
2020-12-17 00:10:20 +0900

    feat: 리덕스에 typescript 도입

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 // import MyForm from "components/MyForm";
-import Counter from "components/Counter";
+// import Counter from "components/Counter";
+import ReducerSample from "components/ReducerSample";
 
 const App: React.FC = () => {
   // const onSubmit = (form: { name: string; description: string }) => {
   //   console.log(form);
   // };
   // return <MyForm onSubmit={onSubmit}></MyForm>;
-  return <Counter></Counter>;
+  return <ReducerSample />;
 };
 
 export default App;
