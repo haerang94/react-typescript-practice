7875a2d30f763f9ec3571bf3f62df6eae79b7115
haerang <gofkdvjvl@naver.com>
2020-12-16 23:49:24 +0900

    feat: counter reducer 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import MyForm from "components/MyForm";
+// import MyForm from "components/MyForm";
+import Counter from "components/Counter";
 
 const App: React.FC = () => {
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
-  return <MyForm onSubmit={onSubmit}></MyForm>;
+  // const onSubmit = (form: { name: string; description: string }) => {
+  //   console.log(form);
+  // };
+  // return <MyForm onSubmit={onSubmit}></MyForm>;
+  return <Counter></Counter>;
 };
 
 export default App;
